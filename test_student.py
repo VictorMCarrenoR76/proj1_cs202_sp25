639f877b99b676ec4412955f4c5b3611d949a31a
VictorMCarrenoR76 <vcarreno@calpoly.edu>
2026-04-20 23:53:51 -0700

    Update test_student.py

--- test_student.py
+++ test_student.py
@@ -1,16 +1,42 @@
+#Victor Manuel Carreno Rodriguez
 import unittest
 from proj1 import *
-#proj1.py should contain your data class and function definitions
-#these do not contribute positivly to your grade. 
-#but your grade will be lowered if they are missing
 
 class TestRegionFunctions(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.rect = GlobeRect(10.0, 20.0, 30.0, 40.0)
+        self.region = Region(self.rect, "Testland", "other")
+        self.rc = RegionCondition(self.region, 2025, 1000, 5000.0)
 
-    def test_holder(self):
-        pass
+    def test_emissions_per_capita(self):
+        self.assertAlmostEqual(emissions_per_capita(self.rc), 5.0, places=4)
+
+    def test_emissions_per_capita_zero(self):
+        rc = RegionCondition(self.region, 2025, 0, 5000.0)
+        self.assertEqual(emissions_per_capita(rc), 0.0)
+
+    def test_area(self):
+        self.assertGreater(area(self.rect), 0)
+
+    def test_emissions_per_square_km(self):
+        self.assertGreater(emissions_per_square_km(self.rc), 0)
+
+    def test_densest(self):
+        rc2 = RegionCondition(self.region, 2025, 2000, 5000.0)
+        self.assertEqual(densest([self.rc, rc2]), "Testland")
+
+    def test_project_condition_year(self):
+        projected = project_condition(self.rc, 5)
+        self.assertEqual(projected.year, 2030)
+
+    def test_project_condition_growth(self):
+        projected = project_condition(self.rc, 10)
+        self.assertGreater(projected.pop, self.rc.pop)
+
+    def test_project_condition_immutable(self):
+        projected = project_condition(self.rc, 5)
+        self.assertNotEqual(projected, self.rc)
 
 
 if __name__ == '__main__':
